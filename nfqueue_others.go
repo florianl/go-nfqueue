6e4adc583677dfb6b7948dd6d27e62f7d186b86d
Lehner Florian <dev@der-flo.net>
2018-11-16 21:04:03 +0100

    Update others
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- nfqueue_others.go
+++ nfqueue_others.go
@@ -4,14 +4,13 @@ package nfqueue
 
 import (
 	"context"
-	"log"
 )
 
 // Nfqueue is not implemented for OS other than linux
 type Nfqueue struct{}
 
 // Open is not implemented for OS other than Linux
-func Open() (*Nfqueue, error) {
+func Open(_ *Config) (*Nfqueue, error) {
 	return nil, ErrNotLinux
 }
 
@@ -21,7 +20,7 @@ func (nfq *Nfqueue) Close() error {
 }
 
 // Register is not implemented for OS other than Linux
-func (nfq *Nfqueue) Register(_ context.Context, _ byte, _ *log.Logger, _ HookFunc) error {
+func (nfe *Nfqueue) Register(_ context.Context, _ byte, _ HookFunc) error {
 	return ErrNotLinux
 }
 
@@ -31,13 +30,18 @@ func (nfq *Nfqueue) SetFlag(_ uint32) error {
 }
 
 // SetVerdict is not implemented for OS other than Linux
-func (nfq *Nfqueue) SetVerdict(_, _ int) (uint32, error) {
-	return 0, ErrNotLinux
+func (nfq *Nfqueue) SetVerdict(_, _ int) error {
+	return ErrNotLinux
 }
 
 // SetVerdictBatch is not implemented for OS other than Linux
-func (nfq *Nfqueue) SetVerdictBatch(_, _ int) (uint32, error) {
-	return 0, ErrNotLinux
+func (nfq *Nfqueue) SetVerdictBatch(_, _ int) error {
+	return ErrNotLinux
+}
+
+// SetVerdictWithMark is not implemented for OS other than Linux
+func (nfqueue *Nfqueue) SetVerdictWithMark(_, _, _ int) error {
+	return ErrNotLinux
 }
 
 // ShowFlags is not implemented for OS other than Linux
