1d761348fa693b99622a53331f3a2cae23a59dda
Lehner Florian <dev@der-flo.net>
2018-12-28 12:26:53 +0100

    Add example
    Signed-off-by: Lehner Florian <dev@der-flo.net>

---
+++ example_test.go
@@ -0,0 +1,52 @@
+//+build linux
+
+package nfqueue_test
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	nfqueue "github.com/florianl/go-nfqueue"
+)
+
+func ExampleNfqueue_Register() {
+	// Send outgoing pings to nfqueue queue 100
+	// # sudo iptables -I OUTPUT -p icmp -j NFQUEUE --queue-num 100
+
+	// Set configuration options for nfqueue
+	config := nfqueue.Config{
+		NfQueue:      100,
+		MaxPacketLen: 0xFFFF,
+		MaxQueueLen:  0xFF,
+		Copymode:     nfqueue.NfQnlCopyPacket,
+	}
+
+	nf, err := nfqueue.Open(&config)
+	if err != nil {
+		fmt.Println("could not open nfqueue socket:", err)
+		return
+	}
+	defer nf.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	fn := func(m nfqueue.Msg) int {
+		id := m[nfqueue.AttrPacketID].(uint32)
+		// Just print out the id and payload of the nfqueue packet
+		fmt.Printf("[%d]\t%v\n", id, m[nfqueue.AttrPayload])
+		nf.SetVerdict(id, nfqueue.NfAccept)
+		return 0
+	}
+
+	// Register your function to listen on nflqueue queue 100
+	err = nf.Register(ctx, fn)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Block till the context expires
+	<-ctx.Done()
+}
