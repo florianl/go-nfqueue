b33ce100dde3ac247586c36b44f7e17c5df5fc5a
Lehner Florian <dev@der-flo.net>
2019-03-26 09:31:02 +0100

    Keep backward compatibility
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- nfqueue.go
+++ nfqueue.go
@@ -6,70 +6,17 @@ import (
 	"context"
 	"encoding/binary"
 	"log"
-	"time"
 
 	"github.com/mdlayher/netlink"
 	"github.com/pkg/errors"
 	"golang.org/x/sys/unix"
 )
 
-// Nfqueue represents a netfilter queue handler
-type Nfqueue struct {
-	// Con is the pure representation of a netlink socket
-	Con *netlink.Conn
-
-	logger *log.Logger
-
-	flags        []byte // uint32
-	maxPacketLen []byte // uint32
-	family       uint8
-	queue        uint16
-	maxQueueLen  []byte // uint32
-	copymode     uint8
-	readTimeout  time.Duration
-	writeTimeout time.Duration
-}
-
 // devNull satisfies io.Writer, in case *log.Logger is not provided
 type devNull struct{}
 
 func (devNull) Write(p []byte) (int, error) {
 	return 0, nil
-}
-
-// Open a connection to the netfilter queue subsystem
-func Open(config *Config) (*Nfqueue, error) {
-	var nfqueue Nfqueue
-
-	if config.Flags >= nfQaCfgFlagMax {
-		return nil, ErrInvFlag
-	}
-
-	con, err := netlink.Dial(unix.NETLINK_NETFILTER, &netlink.Config{NetNS: config.NetNS})
-	if err != nil {
-		return nil, err
-	}
-	nfqueue.Con = con
-	// default size of copied packages to userspace
-	nfqueue.maxPacketLen = []byte{0x00, 0x00, 0x00, 0x00}
-	binary.BigEndian.PutUint32(nfqueue.maxPacketLen, config.MaxPacketLen)
-	nfqueue.flags = []byte{0x00, 0x00, 0x00, 0x00}
-	binary.BigEndian.PutUint32(nfqueue.flags, config.Flags)
-	nfqueue.queue = config.NfQueue
-	nfqueue.family = config.AfFamily
-	nfqueue.maxQueueLen = []byte{0x00, 0x00, 0x00, 0x00}
-	binary.BigEndian.PutUint32(nfqueue.maxQueueLen, config.MaxQueueLen)
-	if config.Logger == nil {
-		nfqueue.logger = log.New(new(devNull), "", 0)
-	} else {
-		nfqueue.logger = config.Logger
-	}
-	nfqueue.copymode = config.Copymode
-
-	nfqueue.readTimeout = config.ReadTimeout
-	nfqueue.writeTimeout = config.WriteTimeout
-
-	return &nfqueue, nil
 }
 
 // Close the connection to the netfilter queue subsystem
@@ -99,50 +46,6 @@ func (nfqueue *Nfqueue) SetVerdict(id uint32, verdict int) error {
 // SetVerdictBatch signals the kernel the next action for a batch of packages till id
 func (nfqueue *Nfqueue) SetVerdictBatch(id uint32, verdict int) error {
 	return nfqueue.setVerdict(id, verdict, true, []byte{})
-}
-
-func (nfqueue *Nfqueue) setVerdict(id uint32, verdict int, batch bool, attributes []byte) error {
-	/*
-		struct nfqnl_msg_verdict_hdr {
-			__be32 verdict;
-			__be32 id;
-		};
-	*/
-
-	if verdict != NfDrop && verdict != NfAccept && verdict != NfStolen && verdict != NfQeueue && verdict != NfRepeat {
-		return ErrInvalidVerdict
-	}
-
-	buf := make([]byte, 4)
-	binary.BigEndian.PutUint32(buf, uint32(id))
-	verdictData := append([]byte{0x0, 0x0, 0x0, byte(verdict)}, buf...)
-	cmd, err := netlink.MarshalAttributes([]netlink.Attribute{
-		{Type: nfQaVerdictHdr, Data: verdictData},
-	})
-	if err != nil {
-		return err
-	}
-	data := putExtraHeader(nfqueue.family, unix.NFNETLINK_V0, nfqueue.queue)
-	data = append(data, cmd...)
-	data = append(data, attributes...)
-	req := netlink.Message{
-		Header: netlink.Header{
-			Flags:    netlink.Request,
-			Sequence: 0,
-		},
-		Data: data,
-	}
-	if batch {
-		req.Header.Type = netlink.HeaderType((nfnlSubSysQueue << 8) | nfQnlMsgVerdictBatch)
-	} else {
-		req.Header.Type = netlink.HeaderType((nfnlSubSysQueue << 8) | nfQnlMsgVerdict)
-	}
-
-	deadline := time.Now().Add(nfqueue.writeTimeout)
-	nfqueue.Con.SetWriteDeadline(deadline)
-	_, sErr := nfqueue.Con.Execute(req)
-	return sErr
-
 }
 
 // Register your own function as callback for a netfilter queue
@@ -194,52 +97,7 @@ func (nfqueue *Nfqueue) Register(ctx context.Context, fn HookFunc) error {
 		return err
 	}
 
-	go func() {
-		defer func() {
-			// unbinding from queue
-			_, err = nfqueue.setConfig(uint8(unix.AF_UNSPEC), seq, nfqueue.queue, []netlink.Attribute{
-				{Type: nfQaCfgCmd, Data: []byte{nfUlnlCfgCmdUnbind, 0x0, 0x0, byte(nfqueue.family)}},
-			})
-			if err != nil {
-				nfqueue.logger.Printf("Could not unbind from queue: %v", err)
-				return
-			}
-		}()
-		for {
-			deadline := time.Now().Add(nfqueue.readTimeout)
-			nfqueue.Con.SetReadDeadline(deadline)
-			replys, err := nfqueue.Con.Receive()
-			if err != nil {
-				nfqueue.logger.Printf("Could not receive message: %v", err)
-				select {
-				case <-ctx.Done():
-					return
-				default:
-					continue
-				}
-			}
-			for _, msg := range replys {
-				if msg.Header.Type == netlink.Done {
-					// this is the last message of a batch
-					// continue to receive messages
-					break
-				}
-				m, err := parseMsg(nfqueue.logger, msg)
-				if err != nil {
-					nfqueue.logger.Printf("Could not parse message: %v", err)
-					continue
-				}
-				if ret := fn(m); ret != 0 {
-					return
-				}
-			}
-			select {
-			case <-ctx.Done():
-				return
-			default:
-			}
-		}
-	}()
+	go nfqueue.socketCallback(ctx, fn, seq)
 
 	return nil
 }
