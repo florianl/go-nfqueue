23037deac730591c5eb572db93ba00ae4f834584
Lehner Florian <dev@der-flo.net>
2019-03-27 10:46:43 +0100

    Add blocking test
    Signed-off-by: Lehner Florian <dev@der-flo.net>

---
+++ nfqueue_linux_gteq_1.12_integration_test.go
@@ -0,0 +1,50 @@
+//+build go1.12,integration,linux
+
+package nfqueue
+
+import (
+	"context"
+	"testing"
+	"time"
+)
+
+func TestTimeout(t *testing.T) {
+
+	timeout := 10 * time.Millisecond
+
+	// Set configuration options for nfqueue
+	config := Config{
+		NfQueue:      123,
+		MaxPacketLen: 0xFFFF,
+		MaxQueueLen:  0xFF,
+		Copymode:     NfQnlCopyPacket,
+		ReadTimeout:  timeout,
+	}
+
+	nfq, err := Open(&config)
+	if err != nil {
+		t.Fatalf("failed to open nfqueue socket: %v", err)
+	}
+	defer nfq.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*timeout)
+	defer cancel()
+
+	fn := func(m Msg) int {
+		id := m[AttrPacketID].(uint32)
+		// Just print out the id and payload of the nfqueue packet
+		t.Logf("[%d]\t%v\n", id, m[AttrPayload])
+		nfq.SetVerdict(id, NfAccept)
+		return 0
+	}
+
+	// Register your function to listen on nflog group 123
+	// This also does a reading on the netlink socket
+	err = nfq.Register(ctx, fn)
+	if err != nil {
+		t.Fatalf("failed to register hook function: %v", err)
+	}
+
+	// Block till the context expires
+	<-ctx.Done()
+}
