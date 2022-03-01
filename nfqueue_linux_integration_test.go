b36f1404dae22f23837d595770118038de903f35
Florian Lehner <dev@der-flo.net>
2022-03-01 20:33:16 +0100

    remove legacy code
    Signed-off-by: Florian Lehner <dev@der-flo.net>

--- nfqueue_linux_integration_test.go
+++ nfqueue_linux_integration_test.go
@@ -61,3 +61,42 @@ func TestLinuxNfqueue(t *testing.T) {
 	// Block till the context expires
 	<-ctx.Done()
 }
+
+func TestTimeout(t *testing.T) {
+	// Set configuration options for nfqueue
+	config := Config{
+		NfQueue:      123,
+		MaxPacketLen: 0xFFFF,
+		MaxQueueLen:  0xFF,
+		Copymode:     NfQnlCopyPacket,
+	}
+
+	nfq, err := Open(&config)
+	if err != nil {
+		t.Fatalf("failed to open nfqueue socket: %v", err)
+	}
+	defer nfq.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Minute)
+
+	fn := func(a Attribute) int {
+		id := *a.PacketID
+		// Just print out the id and payload of the nfqueue packet
+		t.Logf("[%d]\t%v\n", id, *a.Payload)
+		nfq.SetVerdict(id, NfAccept)
+		return 0
+	}
+
+	// Register your function to listen on nflog group 123
+	// This also does a reading on the netlink socket
+	err = nfq.Register(ctx, fn)
+	if err != nil {
+		t.Fatalf("failed to register hook function: %v", err)
+	}
+	// cancel the context to remove the registered hook from the nfqueue.
+	cancel()
+
+	// Block till the context expires
+	<-ctx.Done()
+
+}
